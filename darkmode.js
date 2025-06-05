f7d28ff68c5038084183d1dcf8c2dc400ad88d73
Nermai <r4nerma2@gmail.com>
2025-06-04 22:26:02 -0300

    Add files via upload

---
+++ darkmode.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const toggle = document.getElementById('themeToggle');
+  toggle.addEventListener('click', () => {
+    const currentTheme = document.documentElement.getAttribute('data-theme');
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    document.documentElement.setAttribute('data-theme', newTheme);
+  });
+});
